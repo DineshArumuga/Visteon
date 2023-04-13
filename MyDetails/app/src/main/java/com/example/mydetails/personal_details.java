69d380676b09014dfc6877e7e7b3c210a01b2a75
Dinesh <darumug6@visteon.com>
2023-04-13 11:38:29 +0530

    My Detials App 1.0

---
+++ MyDetails/app/src/main/java/com/example/mydetails/personal_details.java
@@ -0,0 +1,15 @@
+package com.example.mydetails;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class personal_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_details);
+    }
+}
+
